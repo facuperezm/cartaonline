69b4aedad41c388642803e49a12f908c6b9bb2c9
facuperezm <facundoperezmontalvo@gmail.com>
2023-12-15 17:32:19 -0300

    Add sign-up form and password input

--- src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -9,6 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { SignUpForm } from "@/components/auth-sign-up";
 import { OAuthSignIn } from "@/components/auth/oauth-signin";
 import { Shell } from "@/components/shell";
 
@@ -25,6 +26,17 @@ export default async function Signin() {
           </CardHeader>
           <CardContent className="grid gap-4">
             <OAuthSignIn />
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <SignUpForm />
           </CardContent>
           <CardFooter>
             <div className="text-sm text-muted-foreground">
--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -4,6 +4,9 @@ export const authSchema = z.object({
   email: z.string().email({
     message: "Please enter a valid email address",
   }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
 });
 
 export const verifyEmailSchema = z.object({
