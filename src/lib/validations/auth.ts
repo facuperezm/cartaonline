c8ece92e31797b6eacc66475826ad858c08eead4
facuperezm <facundoperezmontalvo@gmail.com>
2023-12-20 20:37:36 -0300

    Refactor sign-up form and add password input field

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -4,6 +4,9 @@ export const authSchema = z.object({
   email: z.string().email({
     message: "Please enter a valid email address",
   }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long",
+  }),
 });
 
 export const verifyEmailSchema = z.object({
