ddd1e35d212aed565cc2b292326b0c6acb9511b0
facuperezm <facundoperezmontalvo@gmail.com>
2024-01-29 22:15:27 -0300

    User not showing on dashboard fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,12 +84,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         <Providers>
-          <body>
-            <div className="absolute top-0 -z-10 h-full w-full bg-white">
-              <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
-            </div>
-            {children}
-          </body>
+          <body>{children}</body>
           <Toaster richColors closeButton />
         </Providers>
       </html>
