54c72145f4bb6b13e39578c12c8d0b270faec750
facuperezm <facundoperezmontalvo@gmail.com>
2024-01-30 17:35:58 -0300

    Add uploadthing dependencies

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,24 @@
+import { currentUser } from "@clerk/nextjs";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async ({}) => {
+      const user = await currentUser();
+
+      if (!user) {
+        throw new Error("Not authenticated");
+      }
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
