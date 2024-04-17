0870c20fa5eb73b1e601b26013a63c4c23cb237a
facuperezm <facundoperezmontalvo@gmail.com>
2024-04-17 16:51:37 -0300

    few changes

--- src/app/(lobby)/pricing/page.tsx
+++ src/app/(lobby)/pricing/page.tsx
@@ -1,22 +1,37 @@
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+import { ArrowRight, Check, Minus } from "lucide-react";
+
 import { PLANS } from "@/config/mercadopago";
 import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 import { Shell } from "@/components/shell";
 
-export default function PricingPage() {
+export default async function PricingPage() {
+  const user = await currentUser();
+
+  if (user) {
+    redirect("/signin");
+  }
+
   const pricingItems = [
     {
       plan: "Free",
       tagline: "Para empezar a vender",
       quota: 10,
-      features: {
-        text: "Hasta 20 productos",
-        footnote: "Todas las funcionalidades de la plataforma",
-      },
+      features: [
+        {
+          text: "Hasta 10 productos",
+          footnote: "Todas las funcionalidades de la plataforma",
+        },
+      ],
     },
     {
       plan: "Pro",
       tagline: "Para negocios grandes",
       quota: PLANS.find((p) => p.slug === "pro")!.quota,
+      features: [],
     },
   ];
 
@@ -26,7 +41,7 @@ export default function PricingPage() {
         <h2 className="text-2xl font-bold leading-tight tracking-tighter">
           Actualizar tienda
         </h2>
-        <p>Elegí que plan se adapta mejor a las necesidades de tu negocio</p>
+        <p>Elegí qué plan se adapta mejor a las necesidades de tu negocio</p>
       </div>
 
       <div className="grid grid-cols-1 gap-10 pt-12 lg:grid-cols-2">
@@ -43,12 +58,12 @@ export default function PricingPage() {
               })}
             >
               {plan === "Pro" && (
-                <div className="absolute -top-5 left-0 right-0 mx-auto w-32 rounded-full bg-gradient-to-r from-blue-600 to-cyan-600 px-3 py-2 text-center text-sm font-medium text-white">
+                <div className="absolute inset-x-0 -top-5 mx-auto w-32 rounded-full bg-gradient-to-r from-blue-600 to-cyan-600 px-3 py-2 text-center text-sm font-medium text-white">
                   Upgrade now
                 </div>
               )}
               <div className="p-5">
-                <h3 className=" my-3 text-center text-3xl font-bold">{plan}</h3>
+                <h3 className="my-3 text-center text-3xl font-bold">{plan}</h3>
                 <p className="text-gray-500">{tagline}</p>
                 <p className="my-5 text-6xl font-semibold">${price}</p>
                 <p className="text-gray-500">per month</p>
@@ -57,6 +72,84 @@ export default function PricingPage() {
                 <div className="flex items-center space-x-1">
                   <p>{quota.toLocaleString()} PDFs/mo included</p>
                 </div>
+              </div>
+
+              <ul className="my-10 space-y-5 px-8">
+                {features.map(
+                  ({
+                    text,
+                    footnote,
+                    negative,
+                  }: {
+                    text: string;
+                    footnote: string;
+                    negative: string;
+                  }) => (
+                    <li key={text} className="flex space-x-5">
+                      <div className="flex-shrink-0">
+                        {negative ? (
+                          <Minus className="h-6 w-6 text-gray-300" />
+                        ) : (
+                          <Check className="h-6 w-6 text-blue-500" />
+                        )}
+                      </div>
+                      {footnote ? (
+                        <div className="flex items-center space-x-1">
+                          <p
+                            className={cn("text-gray-600", {
+                              "text-gray-400": negative,
+                            })}
+                          >
+                            {text}
+                          </p>
+                        </div>
+                      ) : (
+                        <p
+                          className={cn("text-gray-600", {
+                            "text-gray-400": negative,
+                          })}
+                        >
+                          {text}
+                        </p>
+                      )}
+                    </li>
+                  ),
+                )}
+              </ul>
+              <div className="border-t border-gray-200" />
+              <div className="p-5">
+                {plan === "Free" ? (
+                  <Link
+                    href={user ? "/dashboard" : "/sign-in"}
+                    className={buttonVariants({
+                      className: "w-full",
+                      variant: "secondary",
+                    })}
+                  >
+                    {user ? "Upgrade now" : "Sign up"}
+                    <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </Link>
+                ) : user ? (
+                  <Link
+                    href="/upgrade"
+                    className={buttonVariants({
+                      className: "w-full",
+                    })}
+                  >
+                    Upgrade now
+                    <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </Link>
+                ) : (
+                  <Link
+                    href="/sign-in"
+                    className={buttonVariants({
+                      className: "w-full",
+                    })}
+                  >
+                    Sign up
+                    <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </Link>
+                )}
               </div>
             </div>
           );
