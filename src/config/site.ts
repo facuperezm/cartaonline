2707c6dddd36efc08c1cca4cca67f27cf5d06abb
facuperezm <facundoperezmontalvo@gmail.com>
2024-05-25 13:36:29 -0300

    fix: add more info on config file

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,9 @@
 import { type SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "cartaonline.",
+  name: "Carta Online",
   description:
-    "Carta online es una pagina donde podes publicar tu menu para que la gente lo vea antes de ir a comer!",
+    "Carta Online es una pagina donde podes publicar tu menu para que la gente lo vea antes de ir a comer. Crea tu propia carta online y compartila con tus clientes.",
   url: "https://cartaonline.facupm.dev",
   ogImage: "https://cartaonline.facupm.dev/opengraph-image.png",
   links: {
