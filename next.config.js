8d9494cb777c3a647ca52b118e155434631b4459
facuperezm <facundoperezmontalvo@gmail.com>
2024-05-26 19:46:23 -0300

    fix: minor fix with uploadthing permissions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
     unoptimized: true,
   },
 };
