5e0ae918a66f4b7547d92aaadf412101029a9a9a
facuperezm <facundoperezmontalvo@gmail.com>
2024-05-27 00:43:02 -0300

    feat: og image

--- src/app/(lobby)/(routes)/share/[slug]/page.tsx
+++ src/app/(lobby)/(routes)/share/[slug]/page.tsx
@@ -128,7 +128,7 @@ export default async function SlugStorePage({
         </Shell>
         <Link
           className="sticky bottom-5 z-20 mx-auto mb-6 max-w-3xl gap-2 rounded-3xl bg-lime-500 p-2 px-4 text-center text-white shadow-md transition-all hover:bg-lime-500/90"
-          href={`https://web.whatsapp.com/send?phone=${store?.phone}&text=%C2%A1Hola!%20Me%20gustar%C3%ADa%20realizar%20una%20reserva`}
+          href={`https://wa.me/${store?.phone}?text=%C2%A1Hola!%20Me%20gustar%C3%ADa%20realizar%20una%20reserva`}
         >
           <div className="flex gap-2">
             <Icons.whatsapp />
