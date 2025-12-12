71f5f33561f9affbaf8ec44c4f6b41d1fe9d857e
facuperezm <facundoperezmontalvo@gmail.com>
2025-12-12 20:49:25 -0300

    refactor: update padding in DataTable component for improved layout
    - Adjusted padding from 'pb-2 pt-5' to 'py-4' in the DataTable component for a more consistent spacing.
    - This change enhances the visual alignment and overall user experience within the dashboard.

--- src/app/(dashboard)/_components/tables/data-table.tsx
+++ src/app/(dashboard)/_components/tables/data-table.tsx
@@ -71,7 +71,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
-      <div className="flex items-center justify-between pb-2 pt-5">
+      <div className="flex items-center justify-between py-4">
         <div className="flex flex-col  items-start gap-1 md:flex-row">
           <Input
             placeholder="Buscar producto"
